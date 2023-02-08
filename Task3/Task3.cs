98cb33fde15f86b9f6d1ab77c66271fd9601ab39
Aleksey.Alekseev <sadnargl@gmail.com>
2023-02-08 20:32:50 +0300

    задача 23

---
+++ Task3/Task3.cs
@@ -0,0 +1,19 @@
+﻿Console.Write ("Введите число: ");
+if (int.TryParse (Console.ReadLine(), out int i))
+{
+    for (int j = 1; j <= Math.Abs(i); j++) // Math.Abs(i) захватить отрицательные числа
+    {
+        if (i>=0) 
+        {
+        Console.Write ($"{Math.Pow (j,3)} ");
+        }
+        else
+        {
+            Console.Write ($"{Math.Pow (j*(-1),3)} ");
+        }
+    }
+}
+else
+{
+Console.WriteLine ("Вы ввели не числo, либо не целое число, либо слишком большое число");        
+}
